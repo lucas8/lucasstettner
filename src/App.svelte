9b9ef024db57cc904a7cf430d3884712faf51f88
lucas <lucas.stettner@gmail.com>
2023-01-19 11:17:58 -0500

    grammer

--- src/App.svelte
+++ src/App.svelte
@@ -41,7 +41,7 @@
       interfaces for remarkable products.
     </p>
     <p>
-      Previously, I've worked on the future of our education system at <a
+      Previously, I worked on the future of our education system at <a
         href="https://primer.com">Primer</a
       >, was a mentor at
       <a href="https://www.nanohackers.org/">Nano Hacker Academy</a> and helped
