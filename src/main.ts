7273f8e0b9136c7ccf019f16bd68f5153c0ac6d4
lucas <lucas.stettner@gmail.com>
2023-01-18 22:51:33 -0500

    add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import './styles/app.css'
 import './styles/reset.css'
 import App from './App.svelte'
+import { inject } from '@vercel/analytics'
+
+inject()
 
 const app = new App({
   target: document.getElementById('app'),
