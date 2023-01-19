398b862414e4dffa63b92f45f452d44c61b93e5f
lucas <lucas.stettner@gmail.com>
2023-01-18 23:03:50 -0500

    import vercel analytics into html

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,6 @@ import './styles/app.css'
 import './styles/reset.css'
 import App from './App.svelte'
 
-if (process.env.NODE_ENV === 'production') {
-  const { inject } = await import('@vercel/analytics')
-
-  inject()
-}
-
 const app = new App({
   target: document.getElementById('app'),
   intro: true,
